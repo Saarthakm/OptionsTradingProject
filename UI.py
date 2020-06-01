505e09cd625ec6c4c81731c0cd52c1c8fcec67f7
Saarthak Maheshwari <saarthak.m@berkeley.edu>
2020-06-01 17:43:49 -0400

    updates

--- UI.py
+++ UI.py
@@ -7,6 +7,10 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+from kivy.base import runTouchApp
+from kivy.core.image import Image as CoreImage
+
 
 
 
@@ -14,16 +18,17 @@ from kivy.uix.button import Button
 class OptionizeGrid(GridLayout):
     def __init__(self, **kwargs):
         super(OptionizeGrid, self).__init__(**kwargs)
-
-        self.insideGrid = GridLayout()
-        self.insideGrid.cols = 2
-        self.insideGrid.add_widget(Label(text="SPY Premium Trends"))
-        self.insideGrid.add_widget(TextInput(multiline=False))
-        self.add_widget(self.insideGrid)
-        self.cols = 2
-        self.view_SPX = Button(text="SPX Premium Trends")
+        self.cols = 1
+        self.add_widget(Label(text="Optionize"))
+        self.view_SPX = Button(text="View Static Option Premium Trends Plots")
         self.add_widget(self.view_SPX)
         self.view_SPX.bind(on_press = self.premiumPressed)
+        self.view_SP500Dynamic = Button(text="View Live-Time SP500 Option Premium Trend Plots")
+        self.add_widget(self.view_SP500Dynamic)
+        self.logo = CoreImage("OptionizeLogo.png")
+        self.add_widget(self.logo)
+
+
 
     def premiumPressed(self, instance):
         SPXOption = Option("SPX", 'SPX_20180904_to_20180928.csv')
