eb1b2fcaae4465b95d51766c77946f8b1be1ce62
Michael DeProspo <deprosme@dukes.jmu.edu>
2020-08-02 20:45:08 -0400

    Signal line and macd separate and overlayed

--- dataPlt.py
+++ dataPlt.py
@@ -22,7 +22,7 @@ class DataPlt:
             stock = dc(200)
             x = di(self.stock_list, 200)
             stock = dc.stock_create(stock, self.stock_list[val])
-            db = stock.historical_data(20)
+            db = stock.historical_data(60)
             start_date = db.index.min()
             avg_long = di.sma_full_data(stock, stock, 100, 50)
             avg_long = avg_long.truncate(before=start_date)
@@ -30,10 +30,11 @@ class DataPlt:
             avg_short = di.sma_full_data(stock, stock, 100, 5)
             avg_short = avg_short.truncate(before=start_date)
 
-            evm_short = di.ewm_full_data(x, stock, 365, 3)
+            evm_short = di.ewm_full_data(x, stock, 100, 3)
             evm_short = evm_short.truncate(before=start_date)
             mac = di.macd(x, stock)
             mac = mac.truncate(before=start_date)
+            signal = mac.ewm(span=9).mean().fillna('-')
             volume = di.volume_data(x, stock, 365)
             db = db.drop(columns=["open", "volume", "high", "low"])
 
@@ -42,8 +43,10 @@ class DataPlt:
             fig, (ax1, ax2) = plt.subplots(2, sharex=True)
             result.plot(ax=ax1)
             mac.plot(ax=ax2, label='MACD')
+            signal.plot(ax=ax2, label ='Signal Line')
             # plt.plot(ax = ax2, y=0)
             ax1.set_title(stock.ticker)
+            ax2.set_title('MACD and Signal')
             plt.legend(loc='best')
             plt.show()
 
