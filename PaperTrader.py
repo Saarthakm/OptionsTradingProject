c8392d084e677fefe2778d76ec5e064c2c2569d7
Michael DeProspo <deprosme@dukes.jmu.edu>
2020-08-04 20:48:49 -0400

    Renamed due to luis flame

--- PaperTrader.py
+++ PaperTrader.py
@@ -1,4 +1,4 @@
-import alpaca_trade_api as tradeapi
+import alpaca_trade_api as ap
 import threading
 from time import sleep
 import json
@@ -8,7 +8,7 @@ api_key = 'PK3Y5N2FQHN84K1BWSJ7'
 api_secret = '/LhQfCCcu1n5E6eNLVXHVOA3FQhIuaj8JfmjtMYB'
 base_url = 'https://paper-api.alpaca.markets'
 
-api = tradeapi.REST(api_key, api_secret, base_url, api_version='v2')
+api = ap.REST(api_key, api_secret, base_url, api_version='v2')
 
 account = api.get_account()
 print(account)
