b8be8fcc823261edc16cd0b187e3ccc78f9bd972
Luis de los Santos <lfdelossantos@email.wm.edu>
2020-08-17 01:56:19 -0400

    added parameter testing

--- LSTM HYPE.py
+++ LSTM HYPE.py
@@ -9,18 +9,21 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
-
+from tensorflow.keras import activations
 
 class lstm_model:
-    def aaaaa(self, hist, epoch, batch_size, window, dropout, unit1, unit2, unit3, unit4):
+    def aaaaa(self, hist=1000, epoch=100, batch_size=4, window=25, dropout=.5, dropout2=0.5, dropout3=0.5, dropout4=0.5,
+              unit1=500, unit2=500, unit3=500, unit4=500):
 
         stock = st.Stock("AAPL")
 
         dataset = stock.historical_data(hist)
         dataset = dataset.dropna()
         dataset_mid = pd.DataFrame()
-        dataset_mid["avg"] = dataset[['high', 'low']].mean(axis=1)
+        # dataset_mid["avg"] = dataset[['high', 'low']].mean(axis=1)
+        dataset_mid['avg'] = dataset['open']
         timeframe = len(dataset_mid)
+        # test_time_frame = int(timeframe * .20)
         test_time_frame = int(timeframe * .20)
         train_time_frame = timeframe - test_time_frame
 
@@ -33,7 +36,7 @@ class lstm_model:
 
         sc.fit(train_data)
         training_set_scaled = sc.transform(train_data)
-
+        # training_set_scaled = train_data
         smoothing_window = int(smoothing_time * .25)
 
         for di in range(0, smoothing_time, smoothing_window):
@@ -60,16 +63,18 @@ class lstm_model:
         regressor = Sequential()
         regressor.add(LSTM(units=unit1, return_sequences=True, input_shape=(x_train.shape[1], 1)))
         regressor.add(Dropout(dropout))
+
         regressor.add(LSTM(units=unit2, return_sequences=True))
-        regressor.add(Dropout(dropout))
+        regressor.add(Dropout(dropout2))
         regressor.add(LSTM(units=unit3, return_sequences=True))
-        regressor.add(Dropout(dropout))
+        regressor.add(Dropout(dropout3))
         regressor.add(LSTM(units=unit4))
-        regressor.add(Dropout(dropout))
+        regressor.add(Dropout(dropout4))
         regressor.add(Dense(units=1))
+
         # regressor.compile(optimizer='adam', loss='mean_squared_logarithmic_error', metrics=['mse'])
         regressor.compile(optimizer='adam', loss='mean_squared_logarithmic_error', metrics=['accuracy'])
-        history = regressor.fit(x_train, y_train, epochs=epoch, batch_size=batch_size, verbose=0)
+        history = regressor.fit(x_train, y_train, epochs=epoch, batch_size=batch_size, verbose=1)
         # history = regressor.fit(x_train, y_train, epochs=150, batch_size=4, validation_split=.30)
         train_data = pd.DataFrame(train_data)
         test_data = pd.DataFrame(test_data)
@@ -89,11 +94,10 @@ class lstm_model:
         x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
         predicted_stock_price = regressor.predict(x_test)
-        # print(history.history['loss'])
-        # print(history.history['accuracy'])
-        # print(history.history['val_loss'])
-        # print(history.history['val_accuracy'])
+        sc.fit(train_data)
+
         predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
         df = []
         f = 0
         test_data = test_data.to_numpy()
@@ -109,35 +113,32 @@ class lstm_model:
                 f += 1
         length = len(test_data) - 11
         t = length - f
-        return (t / length)
+        print((t / length))
 
-        # predicted_stock_price = pd.DataFrame(predicted_stock_price)
+        predicted_stock_price = pd.DataFrame(predicted_stock_price)
         # predicted_stock_price = predicted_stock_price.iloc[10:]
-        # # real_stock_price = pd.DataFrame(test_data).values
+        real_stock_price = pd.DataFrame(test_data).values
         # real_stock_price = pd.DataFrame(test_data)
-        #
-        # plt.plot(real_stock_price, color='red', label='EMA of Average Price')
-        # plt.plot(predicted_stock_price, color='blue', label='Predicted Price Average')
-        # plt.title('Apple Stock Price Prediction')
-        # plt.xlabel('Time')
-        # plt.ylabel('Exponential Average of Apple''s price ')
-        # # plt.plot(model.history['accuracy'], label = 'training loss')
-        # # plt.plot(model.history['val_acc'], label = 'test loss')
-        # # plt.xlabel("training loss")
-        # # plt.ylabel("test loss")
-        # plt.legend()
-        # plt.show()
+
+        plt.plot(real_stock_price, color='red', label='EMA of Average Price')
+        plt.plot(predicted_stock_price, color='blue', label='Predicted Price Average')
+        plt.title('Apple Stock Price Prediction')
+        plt.xlabel('Time')
+        plt.ylabel('Exponential Average of Apple''s price ')
+
+        plt.legend()
+        plt.show()
 
     def testing_hp(self):
-        hist = [1000, 2000, 3000, 4000, 5000]
-        epoch = [30, 50, 80, 100, 120, 140, 160, 200]
-        batch_size = [4, 8, 16, 32, 64, 128, 256, 512]
-        window = [20, 40, 50, 60, 80, 100]
-        dropout = [.1, .2, .3, .4]
-        unit1 = [4, 6, 8, 10, 15, 20, 30, 40, 50, 75, 100]
-        unit2 = [4, 6, 8, 10, 15, 20, 30, 40, 50, 75, 100]
-        unit3 = [4, 6, 8, 10, 15, 20, 30, 40, 50, 75, 100]
-        unit4 = [4, 6, 8, 10, 15, 20, 30, 40, 50, 75, 100]
+        hist = [2000]
+        epoch = [125]
+        batch_size = [4, 10]
+        window = [30]
+        dropout = [.3]
+        unit1 = [4, 10, 20, 40, 60, 100]
+        unit2 = [4, 10, 20, 40, 60, 100]
+        unit3 = [4, 10, 20, 40, 60, 100]
+        unit4 = [4, 10, 20, 40, 60, 100]
         for h in hist:
             for e in epoch:
                 for b in batch_size:
@@ -155,4 +156,4 @@ class lstm_model:
                                             print("ACCURACY=" + str(self.aaaaa(h, e, b, w, d, u, un, uni, unit)))
 
 a = lstm_model()
-a.testing_hp()
+a.aaaaa()
